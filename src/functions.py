b7a4bbe8b2750e538f7a58fb06da447cefa1861d
GitVexy <erik.appsec@gmail.com>
2025-02-10 20:26:36 +0100

    Reformatted

--- src/functions.py
+++ src/functions.py
@@ -1,87 +1,103 @@
-from htmlnode import *
-from textnode import *
+from htmlnode import LeafNode
+from textnode import TextType, TextNode
 import re as regex
+
 
 def text_node_to_html_node(text_node):
     match text_node.text_type:
         case TextType.TEXT:
-            return LeafNode(tag = None,   value=text_node.text)
-        
+            return LeafNode(tag=None,
+                            value=text_node.text)
         case TextType.BOLD:
-            return LeafNode(tag = "b",    value=text_node.text)
-        
+            return LeafNode(tag="b",
+                            value=text_node.text)
+
         case TextType.ITALIC:
-            return LeafNode(tag = "i",    value=text_node.text)
-        
+            return LeafNode(tag="i",
+                            value=text_node.text)
+
         case TextType.CODE:
-            return LeafNode(tag = "code", value=text_node.text)
-        
+            return LeafNode(tag="code",
+                            value=text_node.text)
+
         case TextType.LINK:
-            return LeafNode(tag = "a",    value=text_node.text, props={"href": text_node.url})
-        
+            return LeafNode(tag="a",
+                            value=text_node.text,
+                            props={"href": text_node.url})
+
         case TextType.IMAGE:
-            return LeafNode(tag = "img",  value = "", props = {"src": text_node.url, "alt": text_node.text})
-        
+            return LeafNode(tag="img",
+                            value="",
+                            props={"src": text_node.url,
+                                   "alt": text_node.text})
+
         case _:
             raise ValueError(f"Unsupported TextType: {text_node.text_type}")
 
-def split_nodes_delimiter(old_nodes: list, delimiter: str, text_type: TextType) -> list:
+
+def split_nodes_delimiter(old_nodes: list,
+                          delimiter: str,
+                          text_type: TextType) -> list:
     new_nodes = []
-    
+
     for node in old_nodes:
-        
+
         if node.text_type != TextType.TEXT:
             new_nodes.append(node)
             continue
-        
+
         sectioned_nodes = []
         sections: list = node.text.split(delimiter)
-        
+
         if len(sections) % 2 == 0:
-            raise ValueError(f"Invalid markdown. Missing closing delimiter\nValue: {node.text}")
-        
+            raise ValueError(
+                f"Invalid markdown. Missing closing delimiter\nValue: {node.text}")
+
         for i in range(len(sections)):
             if not sections[i]:
                 continue
-            
+
             if i % 2 == 0:
                 sectioned_nodes.append(TextNode(sections[i], TextType.TEXT))
-            
+
             else:
                 sectioned_nodes.append(TextNode(sections[i], text_type))
-        
+
         new_nodes.extend(sectioned_nodes)
     return new_nodes
+
 
 def extract_markdown_images(text: str) -> list[tuple]:
     return regex.findall(r"!\[(.*?)\]\((.*?)\)", text)
 
+
 def extract_markdown_links(text: str) -> list[tuple]:
     return regex.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
 
+
 def split_nodes_image(old_nodes: list[TextNode]):
     new_nodes = []
-    
+
     for old_node in old_nodes:
-        
+
         if old_node.text_type != TextType.TEXT:
             new_nodes.append(old_node)
             continue
-        
+
         original_text = old_node.text
         images = extract_markdown_images(original_text)
-        
+
         if len(images) == 0:
             new_nodes.append(old_node)
             continue
-        
+
         for image in images:
             sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
             if len(sections) != 2:
                 raise ValueError("invalid markdown, image section not closed")
             if sections[0] != "":
                 new_nodes.append(TextNode(sections[0], TextType.TEXT))
-                
+
             new_nodes.append(
                 TextNode(
                     image[0],
@@ -89,46 +105,47 @@ def split_nodes_image(old_nodes: list[TextNode]):
                     image[1],
                 )
             )
-            
+
             original_text = sections[1]
-        
+
         if original_text != "":
             new_nodes.append(TextNode(original_text, TextType.TEXT))
-            
+
     return new_nodes
 
 
 def split_nodes_link(old_nodes: list[TextNode]):
     new_nodes = []
-    
+
     for old_node in old_nodes:
         if old_node.text_type != TextType.TEXT:
             new_nodes.append(old_node)
             continue
-        
+
         original_text = old_node.text
         links = extract_markdown_links(original_text)
-        
+
         if len(links) == 0:
             new_nodes.append(old_node)
             continue
-        
+
         for link in links:
             sections = original_text.split(f"[{link[0]}]({link[1]})", 1)
-            
+
             if len(sections) != 2:
                 raise ValueError("invalid markdown, link section not closed")
-            
+
             if sections[0] != "":
                 new_nodes.append(TextNode(sections[0], TextType.TEXT))
-            
+
             new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
             original_text = sections[1]
-        
+
         if original_text != "":
             new_nodes.append(TextNode(original_text, TextType.TEXT))
-        
+
     return new_nodes
+
 
 def text_to_textnodes(text):
     nodes = [TextNode(text, TextType.TEXT)]
@@ -136,25 +153,26 @@ def text_to_textnodes(text):
         ("**", TextType.BOLD),
         ("*", TextType.ITALIC),
         ("`", TextType.CODE)
-        ]
-    
+    ]
+
     for delimiter in delimiters:
         nodes = split_nodes_delimiter(nodes, delimiter[0], delimiter[1])
-    
+
     nodes = split_nodes_image(nodes)
     nodes = split_nodes_link(nodes)
-    
+
     return nodes
+
 
 def markdown_to_blocks(markdown: str) -> list[str]:
     blocks = markdown.split("\n\n")
     output_blocks = []
-    
+
     for block in blocks:
         stripped_block = block.strip()
         if stripped_block:
             output_blocks.append(stripped_block)
-    
+
     return output_blocks
 
 # TODO:
--- src/test_nodes.py
+++ src/test_nodes.py
@@ -1,88 +1,105 @@
 import unittest
 
-from htmlnode   import *
-from textnode   import *
-from functions  import *
+from htmlnode import HTMLNode, LeafNode, ParentNode
+from textnode import TextNode, TextType
+from functions import (
+    text_node_to_html_node,
+    split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links,
+    split_nodes_image,
+    split_nodes_link,
+    text_to_textnodes,
+    markdown_to_blocks)
+
 
 def log(function_name):
     print(f"{function_name}")
+
 
 class TestHTMLNode(unittest.TestCase):
     def run(self, result=None):
         super().run(result)
         log(self._testMethodName)
-    
+
     def setUp(self):
-        child1, child2 = "", "" # To avoid errors. Redundant
+        child1, child2 = "", ""  # To avoid errors. Redundant
         self.node = HTMLNode(tag="p",
-                        value="Hello world",
-                        children=[child1, child2],
-                        props={
-                            "href": "https://www.boot.dev/",
-                            "target": "_blank"})
+                             value="Hello world",
+                             children=[child1, child2],
+                             props={
+                                 "href": "https://www.boot.dev/",
+                                 "target": "_blank"})
         self.empty_node = HTMLNode()
-    
+
     def test_htmlnode_empty(self):
+
         for property in [self.empty_node.tag,
-                        self.empty_node.value,
-                        self.empty_node.children,
-                        self.empty_node.props]:
+                         self.empty_node.value,
+                         self.empty_node.children,
+                         self.empty_node.props]:
+
             self.assertIsNone(property)
-    
+
     def test_htmlnode_has_tag(self):
         self.assertIsNotNone(self.node.tag)
-    
+
     def test_htmlnode_has_value(self):
         self.assertIsNotNone(self.node.value)
-    
+
     def test_htmlnode_has_children(self):
         self.assertIsNotNone(self.node.children)
-    
+
     def test_htmlnode_has_props(self):
         self.assertIsNotNone(self.node.props)
-    
+
     def test_htmlnode_has_repr(self):
         self.assertTrue(self.node.__repr__().__contains__("HTMLNode"))
+
 
 class TestLeafNode(unittest.TestCase):
     def run(self, result=None):
         super().run(result)
         log(self._testMethodName)
-    
+
     def setUp(self):
         self.node = LeafNode(tag="p",
-                        value="Hello world",
-                        props={
-                            "href": "https://www.boot.dev/",
-                            "target": "_blank"})
-    
+                             value="Hello world",
+                             props={
+                                 "href": "https://www.boot.dev/",
+                                 "target": "_blank"})
+
     def test_leafnode_has_tag(self):
         self.assertIsNotNone(self.node.tag)
-    
+
     def test_leafnode_has_value(self):
         self.assertIsNotNone(self.node.value)
-    
+
     def test_leafnode_has_props(self):
         self.assertIsNotNone(self.node.props)
-    
+
     def test_leafnode_has_repr(self):
         self.assertTrue(self.node.__repr__().__contains__("LeafNode"))
+
 
 class TestTextNode(unittest.TestCase):
     def run(self, result=None):
         super().run(result)
         log(self._testMethodName)
-    
+
     def setUp(self):
-        self.node   = TextNode("This is a text node",        TextType.TEXT, "https://www.boot.dev/") # Original
-        self.node2  = TextNode("This is a text node",        TextType.TEXT, "https://www.boot.dev/") # Same
-        self.node3  = TextNode("This is another text node",  TextType.TEXT)                          # Different
-        self.text   = TextNode("text" , TextType.TEXT, "boot.dev")
-        self.bold   = TextNode("bold"   , TextType.BOLD, "boot.dev")
-        self.italic = TextNode("italic" , TextType.ITALIC, "boot.dev")
-        self.code   = TextNode("code"   , TextType.CODE, "boot.dev")
-        self.link   = TextNode("link"   , TextType.LINK, "boot.dev")
-        self.image  = TextNode("image"  , TextType.IMAGE, "boot.dev")
+        self.node = TextNode("This is a text node",
+                             TextType.TEXT, "https://www.boot.dev/")
+        self.node2 = TextNode("This is a text node",
+                              TextType.TEXT, "https://www.boot.dev/")
+        self.node3 = TextNode("This is another text node",
+                              TextType.TEXT)
+        self.text = TextNode("text", TextType.TEXT, "boot.dev")
+        self.bold = TextNode("bold", TextType.BOLD, "boot.dev")
+        self.italic = TextNode("italic", TextType.ITALIC, "boot.dev")
+        self.code = TextNode("code", TextType.CODE, "boot.dev")
+        self.link = TextNode("link", TextType.LINK, "boot.dev")
+        self.image = TextNode("image", TextType.IMAGE, "boot.dev")
         self.html_strings = {
             "text":     "text",
             "bold":     "<b>bold</b>",
@@ -91,7 +108,7 @@ class TestTextNode(unittest.TestCase):
             "link":     "<a href=boot.dev>link</a>",
             "image":    "<img src=boot.dev alt=image></img>"
         }
-    
+
     def test_text_node_to_html_text(self):
         text = text_node_to_html_node(self.text)
         self.assertEqual(text.to_html(), self.html_strings["text"])
@@ -116,80 +133,94 @@ class TestTextNode(unittest.TestCase):
         image = text_node_to_html_node(self.image)
         self.assertEqual(image.to_html(), self.html_strings["image"])
 
-    
     def test_text_node_equal(self):
         self.assertEqual(self.node, self.node2)
-    
+
     def test_text_node_not_equal(self):
         self.assertNotEqual(self.node, self.node3)
-    
+
     def test_text_node_type_text_string(self):
         self.assertIsInstance(self.node.text, str)
-    
+
     def test_text_node_type_text_type_class(self):
         self.assertIsInstance(self.node.text_type, TextType)
-    
+
     def test_text_node_type_url_string(self):
         self.assertIsInstance(self.node.url, str)
-    
+
     def test_text_node_type_url_none(self):
         self.assertIsInstance(self.node3.url, type(None))
+
 
 class TestParentNode(unittest.TestCase):
     def run(self, result=None):
         super().run(result)
         log(self._testMethodName)
-    
+
     def setUp(self):
-        self.child1 = LeafNode(tag="a", value="Child 1", props={"href": "https://www.boot.dev/"})
+        self.child1 = LeafNode(tag="a", value="Child 1", props={
+                               "href": "https://www.boot.dev/"})
         self.child2 = LeafNode(tag="p", value="Child 2")
-        self.node  = ParentNode(tag="h1", children=[self.child1, self.child2], props={"boot": "dev"})
-        self.node2 = ParentNode(tag="h1", children=[self.child1, self.child2], props={"boot": "dev"})
-        self.node3 = ParentNode(tag="h1", children=[self.node, self.child2], props={"boot": "dev"}) # Nested parents
-    
+        self.node = ParentNode(tag="h1", children=[
+                               self.child1, self.child2], props={"boot": "dev"})
+        self.node2 = ParentNode(
+            tag="h1", children=[self.child1, self.child2], props={"boot": "dev"})
+        self.node3 = ParentNode(tag="h1", children=[self.node, self.child2], props={
+                                "boot": "dev"})  # Nested parents
+
     def test_parent_node_nested_parents(self):
         self.assertIsInstance(self.node3.to_html(), str)
-    
+
     def test_parent_node_equal(self):
         self.assertEqual(self.node, self.node2)
-    
+
     def test_parent_node_not_equal(self):
         self.assertNotEqual(self.node, self.node3)
-    
+
     def test_parent_node_has_tag(self):
         self.assertIsNotNone(self.node.tag)
-    
+
     def test_parent_node_has_children(self):
         self.assertIsNotNone(self.node.children)
-    
+
     def test_parent_node_has_props(self):
         self.assertIsNotNone(self.node.props)
-    
+
     def test_parent_node_has_repr(self):
         self.assertTrue(self.node.__repr__().__contains__("ParentNode"))
+
 
 class TestFunctions(unittest.TestCase):
     def run(self, result=None):
         super().run(result)
         log(self._testMethodName)
-    
+
     def setUp(self):
-        self.bold            = TextNode("This is text with a **bolded** word"                 , TextType.TEXT)
-        self.bold_double     = TextNode("This is text with a **bolded** word and **another**" , TextType.TEXT)
-        self.bold_multiword  = TextNode("This is text with a **bolded word** and **another**" , TextType.TEXT)
-        self.italic          = TextNode("This is text with an *italic* word"                  , TextType.TEXT)
-        self.bold_and_italic = TextNode("**bold** and *italic*"                               , TextType.TEXT)
-        self.code            = TextNode("This is text with a `code block` word"               , TextType.TEXT)
-        self.img             = TextNode("This is text with an ![image](IMAGINE)"              , TextType.TEXT)
-        self.imgs            = TextNode("![image](IMAGINE) and ![image2](damn)"               , TextType.TEXT)
-        self.link            = TextNode("This is text with a [link](boot.dev)"                , TextType.TEXT)
-        self.links           = TextNode("[link](boot.dev) and ![nlink](boot.dev)"             , TextType.TEXT)
-    
+        self.bold = TextNode(
+            "Text with a **bolded** word", TextType.TEXT)
+        self.bold_double = TextNode(
+            "Text with a **bolded** word and **another**", TextType.TEXT)
+        self.bold_multiword = TextNode(
+            "Text with a **bolded word** and **another**", TextType.TEXT)
+        self.italic = TextNode(
+            "Text with an *italic* word", TextType.TEXT)
+        self.bold_and_italic = TextNode("**bold** and *italic*", TextType.TEXT)
+        self.code = TextNode(
+            "Text with a `code block` word", TextType.TEXT)
+        self.img = TextNode(
+            "Text with an ![image](IMAGINE)", TextType.TEXT)
+        self.imgs = TextNode(
+            "![image](IMAGINE) and ![image2](damn)", TextType.TEXT)
+        self.link = TextNode(
+            "Text with a [link](boot.dev)", TextType.TEXT)
+        self.links = TextNode(
+            "[link](boot.dev) and ![nlink](boot.dev)", TextType.TEXT)
+
     def test_delim_bold(self):
         new_nodes = split_nodes_delimiter([self.bold], "**", TextType.BOLD)
         self.assertListEqual(
             [
-                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("Text with a ", TextType.TEXT),
                 TextNode("bolded", TextType.BOLD),
                 TextNode(" word", TextType.TEXT),
             ],
@@ -197,10 +228,11 @@ class TestFunctions(unittest.TestCase):
         )
 
     def test_delim_bold_double(self):
-        new_nodes = split_nodes_delimiter([self.bold_double], "**", TextType.BOLD)
+        new_nodes = split_nodes_delimiter(
+            [self.bold_double], "**", TextType.BOLD)
         self.assertListEqual(
             [
-                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("Text with a ", TextType.TEXT),
                 TextNode("bolded", TextType.BOLD),
                 TextNode(" word and ", TextType.TEXT),
                 TextNode("another", TextType.BOLD),
@@ -209,10 +241,11 @@ class TestFunctions(unittest.TestCase):
         )
 
     def test_delim_bold_multiword(self):
-        new_nodes = split_nodes_delimiter([self.bold_multiword], "**", TextType.BOLD)
+        new_nodes = split_nodes_delimiter(
+            [self.bold_multiword], "**", TextType.BOLD)
         self.assertListEqual(
             [
-                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("Text with a ", TextType.TEXT),
                 TextNode("bolded word", TextType.BOLD),
                 TextNode(" and ", TextType.TEXT),
                 TextNode("another", TextType.BOLD),
@@ -224,7 +257,7 @@ class TestFunctions(unittest.TestCase):
         new_nodes = split_nodes_delimiter([self.italic], "*", TextType.ITALIC)
         self.assertListEqual(
             [
-                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("Text with an ", TextType.TEXT),
                 TextNode("italic", TextType.ITALIC),
                 TextNode(" word", TextType.TEXT),
             ],
@@ -232,7 +265,8 @@ class TestFunctions(unittest.TestCase):
         )
 
     def test_delim_bold_and_italic(self):
-        new_nodes = split_nodes_delimiter([self.bold_and_italic], "**", TextType.BOLD)
+        new_nodes = split_nodes_delimiter(
+            [self.bold_and_italic], "**", TextType.BOLD)
         new_nodes = split_nodes_delimiter(new_nodes, "*", TextType.ITALIC)
         self.assertListEqual(
             [
@@ -247,7 +281,7 @@ class TestFunctions(unittest.TestCase):
         new_nodes = split_nodes_delimiter([self.code], "`", TextType.CODE)
         self.assertListEqual(
             [
-                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("Text with a ", TextType.TEXT),
                 TextNode("code block", TextType.CODE),
                 TextNode(" word", TextType.TEXT),
             ],
@@ -284,14 +318,15 @@ class TestFunctions(unittest.TestCase):
 
     def test_split_image(self):
         node = TextNode(
-            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)",
+            "Text with an ![image](https://i.imgur.com/zjjcJKZ.png)",
             TextType.TEXT,
         )
         new_nodes = split_nodes_image([node])
         self.assertListEqual(
             [
-                TextNode("This is text with an ", TextType.TEXT),
-                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode("Text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE,
+                         "https://i.imgur.com/zjjcJKZ.png"),
             ],
             new_nodes,
         )
@@ -304,24 +339,28 @@ class TestFunctions(unittest.TestCase):
         new_nodes = split_nodes_image([node])
         self.assertListEqual(
             [
-                TextNode("image", TextType.IMAGE, "https://www.example.COM/IMAGE.PNG"),
+                TextNode("image", TextType.IMAGE,
+                         "https://www.example.COM/IMAGE.PNG"),
             ],
             new_nodes,
         )
 
     def test_split_images(self):
         node = TextNode(
-            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            ("Text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+             " and another ![second image](https://i.imgur.com/3elNhQu.png)"),
             TextType.TEXT,
         )
         new_nodes = split_nodes_image([node])
         self.assertListEqual(
             [
-                TextNode("This is text with an ", TextType.TEXT),
-                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode("Text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE,
+                         "https://i.imgur.com/zjjcJKZ.png"),
                 TextNode(" and another ", TextType.TEXT),
                 TextNode(
-                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                    "second image", TextType.IMAGE,
+                    "https://i.imgur.com/3elNhQu.png"
                 ),
             ],
             new_nodes,
@@ -329,24 +368,28 @@ class TestFunctions(unittest.TestCase):
 
     def test_split_links(self):
         node = TextNode(
-            "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev) with text that follows",
+            ("Text with a [link](https://boot.dev) and"
+             " [another link](https://blog.boot.dev) with text that follows"),
             TextType.TEXT,
         )
         new_nodes = split_nodes_link([node])
         self.assertListEqual(
             [
-                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("Text with a ", TextType.TEXT),
                 TextNode("link", TextType.LINK, "https://boot.dev"),
                 TextNode(" and ", TextType.TEXT),
-                TextNode("another link", TextType.LINK, "https://blog.boot.dev"),
+                TextNode("another link", TextType.LINK,
+                         "https://blog.boot.dev"),
                 TextNode(" with text that follows", TextType.TEXT),
             ],
             new_nodes,
         )
-    
+
     def test_text_to_textnodes(self):
         nodes = text_to_textnodes(
-            "This is **text** with an *italic* word and a `code block` and an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
+            ("This is **text** with an *italic* word and a `code block` and an"
+             " ![image](https://i.imgur.com/zjjcJKZ.png) and"
+             " a [link](https://boot.dev)")
         )
         self.assertListEqual(
             [
@@ -357,7 +400,8 @@ class TestFunctions(unittest.TestCase):
                 TextNode(" word and a ", TextType.TEXT),
                 TextNode("code block", TextType.CODE),
                 TextNode(" and an ", TextType.TEXT),
-                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode("image", TextType.IMAGE,
+                         "https://i.imgur.com/zjjcJKZ.png"),
                 TextNode(" and a ", TextType.TEXT),
                 TextNode("link", TextType.LINK, "https://boot.dev"),
             ],
@@ -365,17 +409,29 @@ class TestFunctions(unittest.TestCase):
         )
 
     def test_markdown_to_blocks(self):
-        markdown_string = "# This is a heading\n\nThis is a paragraph of text. It has some **bold** and *italic* words inside of it.\n\n* This is the first list item in a list block\n* This is a list item\n* This is another list item\n\n\n* This is difficult"
+        markdown_string = ("""
+# This is a heading\n\n
+This is a paragraph of text. It has some **bold** and *italic* words.
+
+
+* This is the first list item in a list block
+* This is a list item
+* This is another list item\n\n\n
+* This is difficult
+""")
         blocks = markdown_to_blocks(markdown_string)
         self.assertListEqual(
             [
             '# This is a heading',
-            'This is a paragraph of text. It has some **bold** and *italic* words inside of it.',
-            '* This is the first list item in a list block\n* This is a list item\n* This is another list item',
+            'This is a paragraph of text. It has some **bold** '
+            'and *italic* words.',
+            '* This is the first list item in a list block\n'
+            '* This is a list item\n'
+            '* This is another list item',
             '* This is difficult'
             ], blocks
         )
 
+
 if __name__ == "__main__":
     unittest.main()
-
